91de62641d6dd5410f547407196e5c864ef596e6
rb125 <rb@smartypans.io>
2026-03-30 12:47:34 +0530

    localized credentials

--- frontend/app/lib/firebase.ts
+++ frontend/app/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6kRYRIeoPeQNO2udqe_Gmb5w5aUgIPFE",
-  authDomain: "chronos-cinema.firebaseapp.com",
-  projectId: "chronos-cinema",
-  storageBucket: "chronos-cinema.firebasestorage.app",
-  messagingSenderId: "921111098600",
-  appId: "1:921111098600:web:2eac086c6ae5885145855d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
